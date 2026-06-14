376b1c8c1c83e61bf2c8a7a46de08242a3b3358d
sourav-2007-17 <neeruj567@gmail.com>
2026-06-14 23:29:53 +0530

    Refactor pattern menu display and input validation

--- Pattern in c++.cpp
+++ Pattern in c++.cpp
@@ -1,166 +1,180 @@
 #include<iostream>
 using namespace std;
+
 int main(){
-    int n,ch;
-    cout << "1.Number Pattern \n2.Star Pattern \n3.Alphabet Pattern \n 4.Triangle Star \n 5.Double Number Star \n 6.Single Number \n 7.Reverse Number \n 8.Floyd's Triangle \n 9.Inverted \n 10.Number Pyramid \n 11.Hollow Diamond \n";
-    cout << "Enter your choose :";
+    int n, ch;
+    
+    // Display menu with better formatting
+    cout << "\n========== PATTERN MENU ==========\n";
+    cout << "1.  Number Pattern\n";
+    cout << "2.  Star Pattern\n";
+    cout << "3.  Alphabet Pattern\n";
+    cout << "4.  Triangle Star\n";
+    cout << "5.  Double Number Star\n";
+    cout << "6.  Single Number\n";
+    cout << "7.  Reverse Number\n";
+    cout << "8.  Floyd's Triangle\n";
+    cout << "9.  Inverted\n";
+    cout << "10. Number Pyramid\n";
+    cout << "11. Diamond Star\n";
+    cout << "==================================\n";
+    
+    cout << "Enter your choice: ";
     cin >> ch;
-    cout << "Enter Your Number :";
+    
+    // Input validation
+    if (ch < 1 || ch > 11) {
+        cout << "Invalid choice!\n";
+        return 1;
+    }
+    
+    cout << "Enter your number: ";
     cin >> n;
+    
+    // Input validation for n
+    if (n <= 0) {
+        cout << "Number must be positive!\n";
+        return 1;
+    }
+    
     switch(ch){
     case 1:
-    for(int i=1;i<=n;i++){
-
-        for(int j=1;j<=n;j++){
-
-            cout << j << " ";
-
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                cout << j << " ";
+            }
+            cout << endl;
         }
-    cout << endl;
-    }
-    break;
+        break;
+        
     case 2:
-    for(int i=1;i<=n;i++){
-
-        for(int j=1;j<=n;j++){
-
-            cout <<"*" << " ";
-
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                cout << "* ";
+            }
+            cout << endl;
         }
-    cout << endl;
-    }
-    break;
-    case 3:
-    cout << "Enter Your Alphabet :";
-    char let;
-    cin >> let;
-    for(int i=0;i<n;i++){
-
-        for(int j=0;j<n;j++){
-
-            cout << let <<" ";
-
-            let++;
+        break;
+        
+    case 3: {
+        cout << "Enter starting alphabet: ";
+        char let;
+        cin >> let;
+        for(int i=0; i<n; i++){
+            char temp = let; // Reset character for each row
+            for(int j=0; j<n; j++){
+                cout << temp << " ";
+                temp++;
+            }
+            cout << endl;
         }
-    cout << endl;
+        break;
     }
-    break;
+        
     case 4:
-    for(int i=0;i<n;i++){
-
-        for(int j=0;j<i+1;j++){
-
-            cout << "*";
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i+1; j++){
+                cout << "*";
+            }
+            cout << endl;
         }
-    cout << endl;
-    }
-    break;
+        break;
+        
     case 5:
-    for(int i=0;i<n;i++){
-
-        for(int j=0;j<i+1;j++){
-
-            cout << i+1;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i+1; j++){
+                cout << i+1;
+            }
+            cout << endl;
         }
-    cout << endl;
-    }
-    break;
+        break;
+        
     case 6:
-    for(int i=0;i<n;i++){
-
-        for(int j=1;j<=i+1;j++){
-
-            cout << j;
+        for(int i=0; i<n; i++){
+            for(int j=1; j<=i+1; j++){
+                cout << j;
+            }
+            cout << endl;
         }
-    cout << endl;
-    }
-    break;
+        break;
+        
     case 7:
-    for(int i=n;i>0;i--){
-
-        for(int j=i+1;j>0;j--){
-
-            cout << j;
+        for(int i=n; i>0; i--){
+            for(int j=i; j>0; j--){
+                cout << j;
+            }
+            cout << endl;
         }
-    cout << endl;
-    }
-    break;
-    case 8:
-    cout <<"Enter Your Starting Number :";
-    int num;
-    cin >> num;
-    for( int i=0;i<n;i++){
-
-        for(int j=0;j<i+1;j++){
-
-            cout << num << " ";
-
-            num++;
+        break;
+        
+    case 8: {
+        cout << "Enter starting number: ";
+        int num;
+        cin >> num;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i+1; j++){
+                cout << num << " ";
+                num++;
+            }
+            cout << endl;
         }
-    cout << endl;
+        break;
     }
-    break;
+        
     case 9:
-    for(int i = 0; i < n; i++) {
-
-        for(int j = 0; j < i; j++) {
-            cout << " ";
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                cout << " ";
+            }
+            for(int j=0; j<n-i; j++){
+                cout << i+1;
+            }
+            cout << endl;
         }
-
-        for(int j = 0; j < n - i; j++) {
-            cout << i + 1;
-        }
-        cout << endl;
-    }
-    break;
+        break;
+        
     case 10:
-    for(int i=0;i<n;i++){
-
-        for(int j=0;j<n-i-1;j++){
-            cout << " ";
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-i-1; j++){
+                cout << " ";
+            }
+            for(int j=1; j<=i+1; j++){
+                cout << j;
+            }
+            for(int j=i; j>=1; j--){
+                cout << j;
+            }
+            cout << endl;
         }
-        for(int j=1;j<=i+1;j++){
-            cout << j;
+        break;
+        
+    case 11: {
+        // Top half (including middle row)
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-i-1; j++) cout << " ";
+            cout << "*";
+            
+            if(i != 0){
+                for(int j=0; j<2*i-1; j++) cout << " ";
+                cout << "*";
+            }
+            cout << endl;
         }
-        for(int j=i;j>=1;j--){
-            cout << j;
+        
+        // Bottom half
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<=i; j++) cout << " ";
+            cout << "*";
+            
+            if(i != n-2){
+                for(int j=0; j<2*(n-i-2)-1; j++) cout << " ";
+                cout << "*";
+            }
+            cout << endl;
         }
-        cout << endl;
+        break;
     }
-    break;
-    case 11:
-    int n = 5; // You can change this to any odd number for size
-
-    // Top half (including middle row)
-    for (int i = 0; i < n; i++) {
-        // Spaces before the first star
-        for (int j = 0; j < n - i - 1; j++) cout << " ";
-        
-        cout << "*"; // First star
-        
-        if (i != 0) {
-            // Spaces between the two stars
-            for (int j = 0; j < 2 * i - 1; j++) cout << " ";
-            cout << "*"; // Second star
-        }
-        cout << endl;
     }
-
-    // Bottom half
-    for (int i = 0; i < n - 1; i++) {
-        // Spaces before the first star
-        for (int j = 0; j <= i; j++) cout << " ";
-        
-        cout << "*"; // First star
-        
-        if (i != n - 2) {
-            // Spaces between the two stars
-            // Formula: 2 * (n - i - 2) - 1
-            for (int j = 0; j < 2 * (n - i - 2) - 1; j++) cout << " ";
-            cout << "*"; // Second star
-        }
-        cout << endl;
-    }
-}
-return 0;
+    
+    return 0;
 }
